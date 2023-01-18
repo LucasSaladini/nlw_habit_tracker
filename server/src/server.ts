af92197a6480c0b13cee5e344267cd6fe0872e2d
Lucas Saladini <lucassaladini@hotmail.com>
2023-01-18 20:05:22 -0300

    Created first route and installed Prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,13 @@
-console.log('olá')
+import Fastify from "fastify"
+
+const app = Fastify()
+
+app.get('/', () => {
+    return 'Hello world'
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server running')
+})
