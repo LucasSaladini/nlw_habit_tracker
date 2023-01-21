081197fba6cc275b79c34845edecb82d12dfcb1e
Lucas Saladini <lucassaladini@hotmail.com>
2023-01-21 16:23:19 -0300

    Created the summary table component

---
+++ web/src/components/SummaryTable.tsx
@@ -0,0 +1,40 @@
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+
+export function SummaryTable() {
+    return (
+        <div className="w-full flex">
+            {/* Display week days vertically based on const weekDays */}
+            <div className="grid grid-rows-7 grid-flow-row gap-3 mt-10">
+                {weekDays.map((weekDay, i) => {
+                    return (
+                        <div 
+                            key={`${weekDay}-${i}`} 
+                            className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+                        >
+                                {weekDay}
+                        </div>
+                    )
+                })}
+            </div>
+            {/* Squares on the right of each week day */}
+            <div className="grid grid-rows-7 grid-flow-col gap-3 mt-10">
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+                <div className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+            </div>
+        </div>
+    )
+}
