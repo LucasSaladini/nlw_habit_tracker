33ca30d9f2fdfb2addfb27f563fa358f56e3929c
Lucas Saladini <lucassaladini@hotmail.com>
2023-01-24 18:04:06 -0300

    Created the summary for each day

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
+
+import { generateRangeBetweenDates } from '../utils/generate-range-between-dates'
 
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+const datesFromYearStart = generateRangeBetweenDates()
+
+const minimumSummaryDateSizes = 18 * 5
+const amountOfDaysToFill = minimumSummaryDateSizes - datesFromYearStart.length
 
 export function Home() {
     return (
@@ -22,7 +28,31 @@ export function Home() {
                     ))
                 }
             </View>
-            <HabitDay />
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100}}
+            >
+                <View className="flex-row flex-wrap">
+                    {
+                        datesFromYearStart.map(date => (
+                            <HabitDay key={date.toISOString()} />
+                            ))
+                        }
+
+                    {
+                        amountOfDaysToFill > 0 && Array
+                        .from({ length: amountOfDaysToFill })
+                        .map((_, index) => (
+                            <View 
+                            key={index}
+                            className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                            style={{ width: DAY_SIZE, height: DAY_SIZE}}
+                            
+                            />
+                            ))
+                        }
+                </View>
+            </ScrollView>
         </View>
     )
 }
