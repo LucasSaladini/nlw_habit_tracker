38413da8f17d4b902d04a0be32140fb868ccbd45
Lucas Saladini <lucassaladini@hotmail.com>
2023-01-28 13:46:39 -0300

    Configured the route to create new habit and defined the routes typing

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -8,7 +8,7 @@ import { New } from '../screens/New'
 
 export function AppRoutes() {
     return (
-        <Navigator>
+        <Navigator screenOptions={{ headerShown: false }}>
             <Screen 
                 name="home"
                 component={Home}
