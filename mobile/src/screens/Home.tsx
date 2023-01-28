6a4b226f50193fdd7ffcd500c3716389445b7dc6
Lucas Saladini <lucassaladini@hotmail.com>
2023-01-28 15:16:26 -0300

    Created screen to display each day with habits and the date and week day of select square

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,4 +1,5 @@
 import { Text, View, ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 import { generateRangeBetweenDates } from '../utils/generate-range-between-dates'
 
@@ -12,6 +13,7 @@ const minimumSummaryDateSizes = 18 * 5
 const amountOfDaysToFill = minimumSummaryDateSizes - datesFromYearStart.length
 
 export function Home() {
+    const { navigate } = useNavigation()
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
@@ -35,7 +37,10 @@ export function Home() {
                 <View className="flex-row flex-wrap">
                     {
                         datesFromYearStart.map(date => (
-                            <HabitDay key={date.toISOString()} />
+                            <HabitDay 
+                                key={date.toISOString()} 
+                                onPress={() => navigate('habit', { date: date.toISOString() })}
+                            />
                             ))
                         }
 
