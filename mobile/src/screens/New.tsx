21c01c24225aa399894220ac934c898e4ee5749f
Lucas Saladini <lucassaladini@hotmail.com>
2023-01-28 15:01:17 -0300

    Finished create new habit screen

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
-import { ScrollView, Text, TextInput, View } from "react-native";
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { BackButton } from "../components/BackButton";
 import { Checkbox } from "../components/Checkbox";
+import { Feather } from "@expo/vector-icons"
+import colors from "tailwindcss/colors";
 
 const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
 
@@ -18,7 +20,10 @@ export function New() {
 
     return (
         <View className="flex-1 bg-background px-8 pt-16">
-            <ScrollView showsVerticalScrollIndicator={ false }>
+            <ScrollView 
+                showsVerticalScrollIndicator={ false }
+                contentContainerStyle={{ paddingBottom: 100 }}
+            >
                 <BackButton />
                 <Text className="mt-6 text-white font-extrabold text-3xl">
                     Criar hábito
@@ -28,6 +33,9 @@ export function New() {
                 </Text>
                 <TextInput 
                     className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-violet-600"
+                    placeholder="Ex.: Exercícios, Dormir bem etc."
+                    autoCapitalize="sentences"
+                    placeholderTextColor={colors.zinc[400]}
                 />
                 <Text className="font-semibold mt-4 mb-3 text-white text-base">
                     Qual a recorrência?
@@ -42,6 +50,17 @@ export function New() {
                         />       
                     ))
                 }
+                <TouchableOpacity
+                    activeOpacity={0.7}
+                    className="w-full h-14 flex-row items-center justify-center bg-green-600 rounded-md mt-6"
+                >
+                    <Feather
+                        name="check"
+                        size={20}
+                        color={colors.white}
+                    />
+                    <Text className="font-semibold text-base text-white ml-2">Confirmar</Text>
+                </TouchableOpacity>
             </ScrollView>
         </View>
     )
