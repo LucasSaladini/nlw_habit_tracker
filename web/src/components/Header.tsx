4364a087c94c660779f172a8850b3ec4652d4c16
Lucas Saladini <lucassaladini@hotmail.com>
2023-01-28 11:31:50 -0300

    Finished habit modal, missing only to add the recorrence of the habit

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -3,6 +3,7 @@ import * as Dialog from '@radix-ui/react-dialog'
 import { useState } from 'react'
 
 import logoImage from '../assets/logo.svg'
+import { NewHabitForm } from './NewHabitForm'
 
 export function Header() {
     return (
@@ -22,7 +23,10 @@ export function Header() {
                 <Dialog.Close className='absolute right-6 top-6 text-zinc-400 hover:text-zinc-200'>
                   <X size={24} aria-label="Fechar" />
                 </Dialog.Close>
-                Conteúdo do modal
+                <Dialog.Title className='text-3xl leading-tight font-extrabold'>
+                  Criar hábito
+                </Dialog.Title>
+                <NewHabitForm />
               </Dialog.Content>
             </Dialog.Portal>
           </Dialog.Root>
