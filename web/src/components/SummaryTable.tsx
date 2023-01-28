f57e6ae747c90cf98b0aa3b384b0981d38b9657b
Lucas Saladini <lucassaladini@hotmail.com>
2023-01-28 12:03:50 -0300

    Installed clsx and created method to color the inside of the squares based on the amount of habits and the amount of completed habits

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -27,7 +27,13 @@ export function SummaryTable() {
             {/* Squares on the right of each week day */}
             <div className="grid grid-rows-7 grid-flow-col gap-3 mt-11">
                 {summaryDates.map(date => {
-                    return <HabitDay key={date.toString()}/>
+                    return (
+                        <HabitDay 
+                            key={date.toString()}
+                            amount={5} 
+                            completed={Math.random()*5} 
+                        />
+                    )
                 })}
                 {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill}).map((_, i) => {
                     return (
