710dfac3d26b9e209d6dd640b517fab5653e002d
Lucas Saladini <lucassaladini@hotmail.com>
2023-02-01 16:45:55 -0300

    Added validation to verify if the date is in past or current and disabling the possibility to edit a complete/uncompleted habit

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,6 +1,7 @@
 import { Alert, ScrollView, Text, View } from "react-native"
 import { useEffect, useState } from "react"
 import { useRoute } from "@react-navigation/native"
+import clsx from "clsx"
 import dayjs from "dayjs"
 
 import { BackButton } from "../components/BackButton"
@@ -32,6 +33,7 @@ export function Habit() {
   const [completedHabits, setCompletedHabits] = useState<string[]>([])
 
   const parsedDate = dayjs(date)
+  const isDateInPast = parsedDate.endOf("day").isBefore(new Date())
   const dayOfWeek = parsedDate.format("dddd")
   const dayAndMonth = parsedDate.format("DD/MM")
 
@@ -89,13 +91,18 @@ export function Habit() {
           {dayAndMonth}
         </Text>
         <ProgressBar progress={habitsProgress} />
-        <View className="mt-6">
+        <View
+          className={clsx("mt-6", {
+            ["opacity-50"]: isDateInPast
+          })}
+        >
           {dayInfo?.possibleHabits ? (
             dayInfo?.possibleHabits.map((habit) => (
               <Checkbox
                 key={habit.id}
                 title={habit.title}
                 checked={completedHabits.includes(habit.id)}
+                disabled={isDateInPast}
                 onPress={() => handleToggleHabit(habit.id)}
               />
             ))
@@ -103,6 +110,11 @@ export function Habit() {
             <HabitsEmpty />
           )}
         </View>
+        {isDateInPast && (
+          <Text className="text-white mt-10 text-center">
+            Você não pode editar hábitos de uma data passada.
+          </Text>
+        )}
       </ScrollView>
     </View>
   )
