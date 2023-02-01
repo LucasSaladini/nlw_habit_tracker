1d33c8ef402601b27d8896e4359d2a93733d16e3
Lucas Saladini <lucassaladini@hotmail.com>
2023-02-01 16:30:43 -0300

    Fetching all data from backend

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,48 +1,97 @@
-import { ScrollView, Text, View } from "react-native";
-import { useRoute } from "@react-navigation/native";
-import dayjs from "dayjs";
+import { Alert, ScrollView, Text, View } from "react-native"
+import { useEffect, useState } from "react"
+import { useRoute } from "@react-navigation/native"
+import dayjs from "dayjs"
 
-import { BackButton } from "../components/BackButton";
-import { ProgressBar } from "../components/ProgressBar";
-import { Checkbox } from "../components/Checkbox";
+import { BackButton } from "../components/BackButton"
+import { ProgressBar } from "../components/ProgressBar"
+import { Checkbox } from "../components/Checkbox"
+import { Loading } from "../components/Loading"
+import { api } from "../lib/axios"
 
 interface Params {
-    date: string
+  date: string
+}
+
+interface DayInfoProps {
+  completedHabits: string[]
+  possibleHabits: {
+    id: string
+    title: string
+  }[]
 }
 
 export function Habit() {
-    const route = useRoute()
-    const { date } = route.params as Params
+  const route = useRoute()
+  const { date } = route.params as Params
 
-    const parsedDate = dayjs(date)
-    const dayOfWeek = parsedDate.format('dddd')
-    const dayAndMonth = parsedDate.format('DD/MM')
+  const [loading, setLoading] = useState(true)
+  const [dayInfo, setDayInfo] = useState<DayInfoProps | null>(null)
+  const [completedHabits, setCompletedHabits] = useState<string[]>([])
 
-    return (
-        <View className="flex-1 bg-background px-8 pt-16">
-            <ScrollView
-                showsVerticalScrollIndicator={ false }
-                contentContainerStyle={{ paddingBottom: 100 }}
-            >
-                <BackButton />
-                <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
-                    {dayOfWeek}
-                </Text>
-                <Text className="text-white font-extrabold text-3xl">
-                    {dayAndMonth}
-                </Text>
-                <ProgressBar progress={30} />
-                <View className="mt-6">
-                    <Checkbox 
-                        title="Beber 2L de água"
-                        checked={ false }
-                    />
-                    <Checkbox 
-                        title="Caminhar"
-                        checked={ true }
-                    />
-                </View>
-            </ScrollView>
+  const parsedDate = dayjs(date)
+  const dayOfWeek = parsedDate.format("dddd")
+  const dayAndMonth = parsedDate.format("DD/MM")
+
+  async function fetchHabits() {
+    try {
+      setLoading(true)
+
+      const response = await api.get("/day", { params: { date } })
+      setDayInfo(response.data)
+      setCompletedHabits(response.data.completedHabits)
+    } catch (error) {
+      console.log(error)
+      Alert.alert("Ops", "Não foi possível carregar as informações dos hábitos")
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  async function handleToggleHabit(habitId: string) {
+    if (completedHabits.includes(habitId)) {
+      setCompletedHabits((prevState) =>
+        prevState.filter((habit) => habit !== habitId)
+      )
+    } else {
+      setCompletedHabits((prevState) => [...prevState, habitId])
+    }
+  }
+
+  useEffect(() => {
+    fetchHabits()
+  }, [])
+
+  if (loading) {
+    return <Loading />
+  }
+
+  return (
+    <View className="flex-1 bg-background px-8 pt-16">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
+        <Text className="mt-6 text-zinc-400 font-semibold text-base lowercase">
+          {dayOfWeek}
+        </Text>
+        <Text className="text-white font-extrabold text-3xl">
+          {dayAndMonth}
+        </Text>
+        <ProgressBar progress={30} />
+        <View className="mt-6">
+          {dayInfo?.possibleHabits &&
+            dayInfo?.possibleHabits.map((habit) => (
+              <Checkbox
+                key={habit.id}
+                title={habit.title}
+                checked={completedHabits.includes(habit.id)}
+                onPress={() => handleToggleHabit(habit.id)}
+              />
+            ))}
         </View>
-    )
+      </ScrollView>
+    </View>
+  )
 }
